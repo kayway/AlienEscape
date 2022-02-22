0f2f7d4a6555b580444335e6db7615fb57bd93ce
kayway <oblivion4eva@gmail.com>
2022-02-22 12:30:41 +0000

    Initial Commit

---
+++ Source/AlienEscape/AlienEscapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AlienEscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALIENESCAPE_API AAlienEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
